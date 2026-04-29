0176aa78aed2bd2ca17451c7146baa1306862187
won <87519450+wonjongjang@users.noreply.github.com>
2026-04-29 14:22:58 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.00 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
@@ -0,0 +1,13 @@
+def solution(str_list, ex):
+    answer = ''
+    
+    for s in str_list:
+        if ex not in s:
+            answer += s
+            
+    return answer
+
+
+
+# def solution(str_list, ex):
+#     return ''.join(s for s in str_list if ex not in s)

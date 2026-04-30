db67fdc39e39bd31ef520676fa9bdff97e1c30cf
won <87519450+wonjongjang@users.noreply.github.com>
2026-04-30 09:42:05 +0900

    [level 0] Title: 커피 심부름, Time: 0.10 ms, Memory: 9.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -0,0 +1,10 @@
+def solution(order):
+    answer = 0
+    
+    for coffee in order:
+        if 'cafelatte' in coffee:
+            answer += 5000
+        else:
+            answer += 4500
+    
+    return answer

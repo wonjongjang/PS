c1b817a07ca7cfc0143f125f9b3965cc6df6c8e0
won <87519450+wonjongjang@users.noreply.github.com>
2026-05-11 10:14:52 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.00 ms, Memory: 8.96 MB -BaekjoonHub

---
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -0,0 +1,12 @@
+def solution(num_list, n):
+    answer = []
+    
+    for i in range(0, len(num_list), n):
+        answer.append(num_list[i:i + n])
+    
+    return answer
+
+
+
+# def solution(num_list, n):
+#     return [num_list[i:i + n] for i in range(0, len(num_list), n)]
